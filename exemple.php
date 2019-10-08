dbb84f1a6ba10493d163e9efeb3d99db96175024
eurowebpage <32062729+eurowebpage@users.noreply.github.com>
2019-10-08 23:25:42 +0200

    Create exemple.php

---
+++ exemple.php
@@ -0,0 +1,45 @@
+<?php 
+require_once("url.inc.php");
+include(BASE_URL . 'url.php');
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <title>Base Url Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Base url test</title>
+<?php include(BASE_URL . 'include/head.php'); ?>
+</head>
+
+<body>
+<div class="jumbotron text-center">
+  <h1>My First Bootstrap Page</h1>
+  <p>Resize this responsive page to see the effect!</p> 
+</div>
+  
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>Column 1</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Column 2</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Column 3</h3>        
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+  </div>
+</div>
+
+
+
+<?php include(BASE_URL . 'include/footer.php'); ?>
+</body>
+</html>
